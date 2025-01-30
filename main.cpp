c9734ca7f236af0c423c80368f1c8124b8b88085
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2025-01-30 12:53:08 +0200

    completed the game

--- main.cpp
+++ main.cpp
@@ -22,9 +22,9 @@ int main()
 
     int row = -1;    // player has not made a decision yet
     int column = -1; // player has not made a decision yet
+    char winner = ' ';
 
     // iterating through the 9 boards
-
     for (int i = 0; i < 9; i++)
     {
         // print game board
@@ -37,6 +37,12 @@ int main()
         cout << "   |   |   " << endl;
         cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
         cout << "   |   |   " << endl;
+
+        // if there is a winner, we are not going to ask for the input
+        if (winner != ' ')
+        {
+            break;
+        }
 
         // get player input
         cout << "The current player is : " << current_player << endl;
@@ -52,7 +58,7 @@ int main()
             // checking if there is no empty space
             else if (board[row][column] != ' ')
             {
-                cout << "board is full, please try again!" << endl;
+                cout << "tile is full, please try again!" << endl;
             }
             else
             {
@@ -69,5 +75,43 @@ int main()
         board[row][column] = current_player;
         // if current player is equal to player_x, then we set the current_player to player_o, otherwise to player_x
         current_player = (current_player == player_x) ? player_0 : player_x;
+
+        // check for winners:
+        //  checking all the rows/horizontal:
+        for (int j = 0; j < 3; j++)
+        {
+            if (board[j][0] != ' ' && board[j][0] == board[j][1] && board[j][1] == board[j][2])
+            {
+                winner = board[j][0];
+                break;
+            }
+        }
+        // checking the columns/verticals
+        for (int k = 0; k < 3; k++)
+        {
+            if (board[0][k] != ' ' && board[0][k] == board[1][k] && board[1][k] == board[2][k])
+            {
+                winner = board[k][0];
+                break;
+            }
+        }
+
+        // checking the diagonals
+        if (board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2])
+        {
+            winner = board[0][0];
+        }
+       else if (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0])
+        {
+            winner = board[0][2];
+        }
+    }
+    if (winner != ' ')
+    {
+        cout << "The winner is player: "<<winner << endl;
+    }
+    else
+    {
+        cout << "Its a Tie" << endl;
     }
 }
